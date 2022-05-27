ed1f901f1b42174c62c10875a35a7e0f32889a0a
dovydaszemait <dovydas.zemas@gmail.com>
2022-05-27 22:14:18 +0300

    adding stripe

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
