6bcc9e539b8f6fcff925d90c17fce54d443d49d1
dovydaszemait <dovydas.zemas@gmail.com>
2022-05-29 23:28:32 +0300

    fixing erros and bugs

--- models/User.js
+++ models/User.js
@@ -9,9 +9,39 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    img: { type: String },
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
+
+// const mongoose = require("mongoose");
+// const jwt = require("jsonwebtoken");
+// const Joi = require("joi");
+// const passwordComplexity = require("joi-password-complexity");
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true },
+//   email: { type: String, required: true },
+//   password: { type: String, required: true },
+// });
+
+// userSchema.methods.generateAuthToken = function () {
+//   const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+//     expiresIn: "7d",
+//   });
+//   return token;
+// };
+
+// const Users = mongoose.model("user", userSchema);
+
+// const validate = (data) => {
+//   const schema = Joi.object({
+//     username: Joi.string().required().label("Username"),
+//     email: Joi.string().email().required().label("Email"),
+//     password: passwordComplexity().required().label("Password"),
+//   });
+//   return schema.validate(data);
+// };
+
+// module.exports = { Users, validate };
